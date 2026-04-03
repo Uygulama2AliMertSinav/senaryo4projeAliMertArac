cf3294d2c4f476ef28f451ee331e9caf2048f194
AliMertTosun <tosunalimert470@gmail.com>
2026-04-03 16:17:18 +0300

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using AliMertTosunAracSinavi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AliMertTosunAracSinavi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Kiralama> Kiralamalar { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // ✅ Admin kullanıcı ekleme
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = 1,
+                    FullName = "Admin",
+                    Email = "admin@gmail.com",
+                    Password = "123456", // Test şifresi
+                    IsAdmin = true
+                }
+            );
+        }
+    }
+}
